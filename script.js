fdb5afeb731e2a0c30cdffe4d2179e8bd4a1bea1
Justin Perlman <justincperlman@gmail.com>
2025-03-20 18:00:58 -0400

    fixed images

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const locations = [
-    { name: "New York City, New York", message: "Walking through Times Square and exploring Central Park! 🏙️", images: ["nyc1.jpg", "nyc2.jpg", "nyc3.jpg", "nyc4.jpg", "nyc5.jpg"], coords: [40.7128, -74.0060] },
-    { name: "Cincinnati, Ohio", message: "Eating chili and enjoying the riverfront views! 🌉", images: ["cincinnati1.jpg", "cincinnati2.jpg", "cincinnati3.jpg", "cincinnati4.jpg", "cincinnati5.jpg"], coords: [39.1031, -84.5120] },
-    { name: "Atlanta, Georgia", message: "Visiting the Georgia Aquarium and grabbing some Southern BBQ! 🍑", images: ["atlanta1.jpg", "atlanta2.jpg", "atlanta3.jpg", "atlanta4.jpg", "atlanta5.jpg"], coords: [33.7490, -84.3880] },
-    { name: "Louisville, Kentucky", message: "Derby time! And exploring the bourbon trail 🏇🥃", images: ["louisville1.jpg", "louisville2.jpg", "louisville3.jpg", "louisville4.jpg", "louisville5.jpg"], coords: [38.2527, -85.7585] },
-    { name: "New Orleans, Louisiana", message: "Jazz, beignets, and a stroll down Bourbon Street 🎷🍩", images: ["neworleans1.jpg", "neworleans2.jpg", "neworleans3.jpg", "neworleans4.jpg", "neworleans5.jpg"], coords: [29.9511, -90.0715] },
-    { name: "Pigeon Forge, Tennessee", message: "Riding roller coasters at Dollywood! 🎢", images: ["pigeonforge1.jpg", "pigeonforge2.jpg", "pigeonforge3.jpg", "pigeonforge4.jpg", "pigeonforge5.jpg"], coords: [35.7884, -83.5543] },
-    { name: "Panama City Beach, Florida", message: "Sun, sand, and beautiful sunsets 🌅", images: ["pcb1.jpg", "pcb2.jpg", "pcb3.jpg", "pcb4.jpg", "pcb5.jpg"], coords: [30.1766, -85.8055] }
+    { name: "New York City, New York", folder: "nyc", message: "Walking through Times Square and exploring Central Park! 🏙️", images: ["NYC1.JPG", "NYC2.JPG", "NYC3.JPG", "NYC4.JPG", "NYC5.JPG"], coords: [40.7128, -74.0060] },
+    { name: "Cincinnati, Ohio", folder: "cincinnati", message: "Eating chili and enjoying the riverfront views! 🌉", images: ["cincinnati1.jpg", "cincinnati2.jpg", "cincinnati3.jpg", "cincinnati4.jpg", "cincinnati5.jpg"], coords: [39.1031, -84.5120] },
+    { name: "Atlanta, Georgia", folder: "atlanta", message: "Visiting the Georgia Aquarium and grabbing some Southern BBQ! 🍑", images: ["atlanta1.jpg", "atlanta2.jpg", "atlanta3.jpg", "atlanta4.jpg", "atlanta5.jpg"], coords: [33.7490, -84.3880] },
+    { name: "Louisville, Kentucky", folder: "louisville", message: "Derby time! And exploring the bourbon trail 🏇🥃", images: ["louisville1.jpg", "louisville2.jpg", "louisville3.jpg", "louisville4.jpg", "louisville5.jpg"], coords: [38.2527, -85.7585] },
+    { name: "New Orleans, Louisiana", folder: "neworleans", message: "Jazz, beignets, and a stroll down Bourbon Street 🎷🍩", images: ["neworleans1.jpg", "neworleans2.jpg", "neworleans3.jpg", "neworleans4.jpg", "neworleans5.jpg"], coords: [29.9511, -90.0715] },
+    { name: "Pigeon Forge, Tennessee", folder: "pigeonforge", message: "Riding roller coasters at Dollywood! 🎢", images: ["pigeonforge1.jpg", "pigeonforge2.jpg", "pigeonforge3.jpg", "pigeonforge4.jpg", "pigeonforge5.jpg"], coords: [35.7884, -83.5543] },
+    { name: "Panama City Beach, Florida", folder: "pcb", message: "Sun, sand, and beautiful sunsets 🌅", images: ["pcb1.jpg", "pcb2.jpg", "pcb3.jpg", "pcb4.jpg", "pcb5.jpg"], coords: [30.1766, -85.8055] }
 ];
 
 const map = L.map('map').setView([37.0902, -95.7129], 5);
@@ -20,9 +20,12 @@ locations.forEach(location => {
 
         location.images.forEach(img => {
             let imgElement = document.createElement("img");
-            imgElement.src = `images/${location.name.toLowerCase().replace(/[^a-z]/g, '')}/${img}`;
+            imgElement.src = `images/${location.folder}/${img}`;
             imgElement.alt = location.name;
             imageGallery.appendChild(imgElement);
         });
     });
 });
+
+
+//images\nyc\NYC1.jpg
